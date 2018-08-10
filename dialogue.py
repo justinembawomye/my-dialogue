15a974f6189cd7806ca81da081e18e3f935c6a40
justinembawomye <jmbawomye@gmail.com>
2018-08-10 17:03:54 +0300

    create dialogue

---
+++ dialogue.py
@@ -0,0 +1,57 @@
+print("Hello! Welcome! Welcome!")
+name = input("Enter your name:")
+funny_fact = input("What is the funny fact about you?")
+One_truth_about_you = input("Tell me one truth about you:")
+One_lie_about_you = input("Tell me one lie about you:")
+color = input("What is your favorite color?")
+
+print(f"Hey {name} How's your day?" )
+answer = input("good or bad: ")
+if (answer == "good"):
+    print(f"Awesome! {name} Enjoy the rest of it")
+if (answer == "Bad"):
+    	print(f"Sorry, {name} Lets hope it will get better!")
+if (answer == "bad"):
+	print(f"Sorry, {name} Lets hope it will get better!")   
+
+if (answer == "Good"):
+    print(f"Awesome! {name} Enjoy the rest of it")    
+
+if (answer == "cool"):
+    print(f"Awesome! {name} Enjoy the rest of it")        
+
+
+if (answer == "awesome"):
+    print(f"Awesome! {name} Enjoy the rest of it") 
+
+if (answer == "Fantastic"):
+    print(f"Awesome! {name} Enjoy the rest of it")       
+
+if (answer == "well"):
+    print(f"Awesome! {name} Enjoy the rest of it")    
+
+input("**Press Enter to Continue**: ")
+print("Great! Lets see if I still remember your favorite color! **Press Enter to continue**")
+
+print(f"I think its {color}")
+
+Color_input = input("Yes or No: ")
+if (Color_input == "Yes"):
+    print("Yeah!!! I got it right.....")
+
+if (Color_input == "yes"):
+    print("Yeah!!! I got it right.....")    
+if (Color_input == "No"):
+    print("Ooh no.... Guess again") 
+if (Color_input == "no"):
+    print("Ooh no.... Don't worry next time better Guess again") 
+
+input("**Press Enter to Continue**: ")
+print(f"It has been a pleasure chatting with you {name} and my favorite color too is {color}")  
+
+input("**Press Enter to Continue**: ")
+print("Thank you and goodbye!!")
+input("***Press CTRL + C to close the program***")     
+
+
+         
